9f5694fd157ce0479e9a39a95fc4df8dcfba04ed
Zihao Xu <zihao.xu@columbia.edu>
2022-09-20 09:54:29 +0800

    clean up and add example

--- appletree/flex/plugin/__init__.py
+++ appletree/flex/plugin/__init__.py
@@ -2,8 +2,10 @@ from . import common
 from . import microphys
 from . import detector
 from . import reconstruction
+from . import efficiency
 
 from .common import *
 from .microphys import *
 from .detector import *
 from .reconstruction import *
+from .efficiency import *
