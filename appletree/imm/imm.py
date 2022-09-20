b3a299329a08898c1036137289276461ef3ac4f4
Zihao Xu <zihao.xu@columbia.edu>
2022-09-21 03:45:48 +0800

    map manager

--- appletree/imm/imm.py
+++ appletree/imm/imm.py
@@ -9,6 +9,7 @@ class Map(object):
         self.map = jnp.asarray(self.data['map'], dtype=float)
         self.coordinate_name = self.data['coordinate_name']
 
+        
 class MapRegBin(object):
     def __init__(self, file_name):
         with open(file_name, 'r') as file:
@@ -18,6 +19,33 @@ class MapRegBin(object):
         self.map = jnp.asarray(self.data['map'], dtype=float)
         self.coordinate_name = self.data['coordinate_name']
         
-class InputMapManager(object):
+        
+class MapManager(object):
     def __init__(self):
-        pass
+        self._maps = {}
+        
+    def register_json_map(self, file_name, coord_type, map_name):
+        if coord_type=='point':
+            map_cls = Map(file_name)
+        elif coord_type=='regbin':
+            map_cls = MapRegBin(file_name)
+        else:
+            raise ValueError("map_type must be either 'default' or 'regbin'!")
+        
+        item = {
+            'map' : map_cls,
+            'bin_type' : coord_type,
+            'file_name' : file_name
+        }
+        
+        self._maps[map_name] = item
+        
+    def get_registration(self):
+        registration = {map_name : map_dict['file_name'] for map_name, map_dict in self._maps.items()}
+        return registration
+    
+    def get_map(self, map_name):
+        return self._maps[map_name]['map']
+        
+    def check_map_exist(self, map_name):
+        return map_name in self._maps
