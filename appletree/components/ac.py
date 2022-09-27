ec21b2bc984c060ed133992e558e9233036390c8
Zihao Xu <zihao.xu@columbia.edu>
2022-09-28 04:05:39 +0800

    rm some compile args

--- appletree/components/ac.py
+++ appletree/components/ac.py
@@ -5,3 +5,5 @@ from appletree.share import DATAPATH
 
 class AC(ComponentFixed):
     file_name = os.path.join(DATAPATH, 'data_XENONnT_Rn220_v8_strax_v1.2.2_straxen_v1.7.1_cutax_v1.9.0.csv')
+    rate_par_name = 'ac_rate'
+    norm_type = 'on_pdf'
