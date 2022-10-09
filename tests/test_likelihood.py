83b925963306d344f999f11ba9b9d983a8c02458
Zihao Xu <zihao.xu@columbia.edu>
2022-10-09 19:07:37 -0400

    Add test scripts (#24)
    * rm old files
    
    * fix bug of key as a tuple
    
    * test component
    
    * rename test scripts
    
    * rename tes functions st pytest can find them
    
    * make dog happy
    
    * doc string and llh test
    
    * happy dog
    
    * context test
    
    * fix emcee bug
    
    * apply batch_size
    
    * docstring and reduce sbatch_size in test

---
+++ tests/test_likelihood.py
@@ -0,0 +1,33 @@
+import os
+import appletree as apt
+
+# Get parameters
+par_config_file_name = os.path.join(apt.PARPATH, 'apt_sr0_er.json')
+par_manager = apt.Parameter(par_config_file_name)
+par_manager.sample_init()
+parameters = par_manager.get_all_parameter()
+
+
+def test_likelihood():
+    """Test Likelihood"""
+    config = dict(
+        data_file_name = os.path.join(
+            apt.share.DATAPATH,
+            'data_XENONnT_Rn220_v8_strax_v1.2.2_straxen_v1.7.1_cutax_v1.9.0.csv',
+        ),
+        bins_type = 'equiprob',
+        bins_on = ['cs1', 'cs2'],
+        bins = [15, 15],
+        x_clip = [0, 100],
+        y_clip = [2e2, 1e4],
+    )
+    llh = apt.Likelihood(**config)
+    llh.register_component(apt.components.AC, 'rn220_ac')
+    llh.register_component(apt.components.ERBand, 'rn220_er')
+    llh.print_likelihood_summary(short=True)
+
+    parameters['rn220_ac_rate'] = parameters['ac_rate']
+    parameters['rn220_er_rate'] = parameters['er_rate']
+
+    key = apt.randgen.get_key()
+    llh.get_log_likelihood(key, int(1e6), parameters)
