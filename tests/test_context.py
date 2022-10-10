546f12c511d7b1eeaa4a2ee585d09e7e30d71cc5
dachengxu <dx2227@columbia.edu>
2022-10-10 22:38:10 +0800

    Prepare to change the configuration system
    Add one more test

--- tests/test_context.py
+++ tests/test_context.py
@@ -2,9 +2,17 @@ import os
 import appletree as apt
 
 
-def test_context():
-    """Test Context"""
+def test_rn220_ar37_context():
+    """Test Context of Rn220 & Ar37 combine fitting"""
     config = os.path.join(apt.share.CONFPATH, 'apt_config_rn220_ar37_sr0.json')
+    context = apt.Context(config)
+
+    context.print_context_summary()
+    context.fitting(nwalkers=100, iteration=2, batch_size=int(1e4))
+
+def test_rn220_context():
+    """Test Context of Rn220 combine fitting"""
+    config = os.path.join(apt.share.CONFPATH, 'apt_config_rn220_sr0.json')
     context = apt.Context(config)
 
     context.print_context_summary()
