a2b3d9a629fad8f8c31738f375ee7ddf13d6f26c
dachengxu <dx2227@columbia.edu>
2022-10-11 21:37:25 +0800

    Remove file name evil

--- appletree/share.py
+++ appletree/share.py
@@ -1,10 +1,2 @@
-import os
-import inspect
-
-# MAPPATH = os.path.join(os.path.abspath(os.path.dirname(inspect.getfile(inspect.currentframe()))), 'maps')
-# DATAPATH = os.path.join(os.path.abspath(os.path.dirname(inspect.getfile(inspect.currentframe()))), 'data')
-# PARPATH = os.path.join(os.path.abspath(os.path.dirname(inspect.getfile(inspect.currentframe()))), 'parameters')
-# CONFPATH = os.path.join(os.path.abspath(os.path.dirname(inspect.getfile(inspect.currentframe()))), 'configs')
-
 _cached_configs = dict()
 _cached_functions = dict()
