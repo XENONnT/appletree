036f7271ae817e5084efa4366cbfca5845560bfa
dachengxu <dx2227@columbia.edu>
2022-12-23 13:26:56 +0800

    Minor change

--- tests/test_context.py
+++ tests/test_context.py
@@ -2,16 +2,6 @@ import appletree as apt
 from appletree.utils import get_file_path
 
 
-def test_rn220_ar37_context():
-    """Test Context of Rn220 & Ar37 combine fitting"""
-    config = get_file_path('rn220_ar37.json')
-    context = apt.Context(config)
-
-    context.print_context_summary()
-    context.fitting(nwalkers=100, iteration=2, batch_size=int(1e4))
-    context.dump_post_parameters('_temp.json')
-
-
 def test_rn220_context():
     """Test Context of Rn220 combine fitting"""
     config = get_file_path('rn220.json')
