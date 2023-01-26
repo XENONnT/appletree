1be0056d2ba1c706be7c29c7bc83217b9aa961c7
dachengxu <dx2227@columbia.edu>
2023-01-27 00:06:27 +0800

    No need to specify Ly & Qy only to NR

--- appletree/plugins/__init__.py
+++ appletree/plugins/__init__.py
@@ -4,8 +4,8 @@ from .common import *
 from . import er_microphys
 from .er_microphys import *
 
-from . import nr_lyqy
-from .nr_lyqy import *
+from . import lyqy
+from .lyqy import *
 
 from . import detector
 from .detector import *
