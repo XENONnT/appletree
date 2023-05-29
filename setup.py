7c8d3f2459e4224cd74c14779c3a5e47805bfab9
dachengxu <dx2227@columbia.edu>
2023-05-29 12:30:41 +0800

    Bump version: 0.2.2 → 0.2.3

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open('HISTORY.md') as file:
 
 setuptools.setup(
     name='appletree',
-    version='0.2.2',
+    version='0.2.3',
     description='A high-Performance Program simuLatEs and fiTs REsponse of xEnon.',
     author='Appletree contributors, the XENON collaboration',
     long_description=readme + '\n\n' + history,
