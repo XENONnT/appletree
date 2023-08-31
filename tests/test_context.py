ae7a7840fe05d5bd78baf160c1225403289608f9
Dacheng Xu <dx2227@columbia.edu>
2023-08-31 00:46:01 -0400

    Add function to check the usage of configs, check_unused_configs (#112)

--- tests/test_context.py
+++ tests/test_context.py
@@ -1,9 +1,11 @@
 import appletree as apt
-from appletree.utils import get_file_path
+from appletree.share import _cached_configs
+from appletree.utils import get_file_path, check_unused_configs
 
 
 def test_rn220_context():
     """Test Context of Rn220 combine fitting"""
+    _cached_configs.clear()
     context = apt.ContextRn220()
 
     context.print_context_summary()
@@ -11,10 +13,12 @@ def test_rn220_context():
     context.dump_post_parameters('_temp.json')
 
     context['rn220_llh']['rn220_er'].new_component()
+    check_unused_configs()
 
 
 def test_rn220_ar37_context():
     """Test Context of Rn220 & Ar37 combine fitting"""
+    _cached_configs.clear()
     context = apt.ContextRn220Ar37()
 
     context.print_context_summary()
@@ -24,19 +28,23 @@ def test_rn220_ar37_context():
 
     parameters = context.get_post_parameters()
     context.get_num_events_accepted(parameters, batch_size=batch_size)
+    check_unused_configs()
 
 
 def test_neutron_context():
     """Test Context of neutron combine fitting"""
+    _cached_configs.clear()
     instruct = get_file_path('neutron_low.json')
     context = apt.Context(instruct)
 
     context.print_context_summary()
     context.fitting(nwalkers=100, iteration=2, batch_size=int(1e4))
+    check_unused_configs()
 
 
 def test_literature_context():
     """Test Context of neutron combine fitting"""
+    _cached_configs.clear()
     instruct = get_file_path('literature_lyqy.json')
     context = apt.Context(instruct)
 
@@ -47,3 +55,4 @@ def test_literature_context():
 
     parameters = context.get_post_parameters()
     context.get_num_events_accepted(parameters, batch_size=batch_size)
+    check_unused_configs()
