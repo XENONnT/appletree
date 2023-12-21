f2e144f9e31cd0fb105e20bced9ec55272eb6916
dachengx <dx2227@columbia.edu>
2023-12-20 19:36:51 -0600

    Update conf, add napoleon

---
+++ docs/source/build_release_notes.py
@@ -0,0 +1,48 @@
+# Copied from straxen/docs/source/build_release_notes.py
+# https://github.com/XENONnT/straxen/blob/a34a02393001f13755c3f84d15924f83bc86c4db
+# /docs/source/build_release_notes.py
+
+import os
+
+from m2r import convert
+
+header = """
+Release notes
+==============
+
+"""
+
+
+def convert_release_notes():
+    """Convert the release notes to an RST page with links to PRs."""
+    this_dir = os.path.dirname(os.path.realpath(__file__))
+    notes = os.path.join(this_dir, "..", "..", "HISTORY.md")
+    with open(notes, "r") as f:
+        notes = f.read()
+    rst = convert(notes)
+    with_ref = ""
+    for line in rst.split("\n"):
+        # Get URL for PR
+        if "#" in line:
+            pr_number = line.split("#")[1]
+            while len(pr_number):
+                try:
+                    pr_number = int(pr_number)
+                    break
+                except ValueError:
+                    # Too many tailing characters to be an int
+                    pr_number = pr_number[:-1]
+            if pr_number:
+                line = line.replace(
+                    f"#{pr_number}",
+                    f"`#{pr_number} <https://github.com/XENONnT/appletree/pull/{pr_number}>`_",
+                )
+        with_ref += line + "\n"
+    target = os.path.join(this_dir, "reference", "release_notes.rst")
+
+    with open(target, "w") as f:
+        f.write(header + with_ref)
+
+
+if __name__ == "__main__":
+    convert_release_notes()
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,10 +14,12 @@ version = apt.__version__
 # -- General configuration
 
 extensions = [
+    "nbsphinx",
     "sphinx.ext.autodoc",
     "sphinx.ext.intersphinx",
     "sphinx.ext.viewcode",
-    "nbsphinx",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.todo",
 ]
 
 intersphinx_mapping = {
@@ -28,9 +30,42 @@ intersphinx_disabled_domains = ["std"]
 
 templates_path = ["_templates"]
 
+# -- Options for NAPOLEON output
+
+napoleon_include_init_with_doc = True
+napoleon_include_private_with_doc = True
+napoleon_include_special_with_doc = True
+
 # -- Options for HTML output
 
 html_theme = "sphinx_rtd_theme"
 
 # -- Options for EPUB output
 epub_show_urls = "footnote"
+
+# -- Options for TODO output
+
+todo_include_todos = True
+
+# These folders are copied to the documentation's HTML output
+html_static_path = ["_static"]
+
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = [
+    "css/custom.css",
+]
+
+
+def setup(app):
+    # app.add_css_file('css/custom.css')
+    # Hack to import something from this dir. Apparently we're in a weird
+    # situation where you get a __name__  is not in globals KeyError
+    # if you just try to do a relative import...
+    import os
+    import sys
+
+    sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+    from build_release_notes import convert_release_notes
+
+    convert_release_notes()
