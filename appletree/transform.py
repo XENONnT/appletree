01175303686513fe7123f561d6c30e680d61a6d2
Zihao Xu <zihao.xu@columbia.edu>
2024-03-09 06:39:49 +0800

    add base

---
+++ appletree/transform.py
@@ -0,0 +1,12 @@
+class Transfromer():
+    def __call__(self, context):
+        return context
+    
+    def fit(self, args):
+        pass
+
+    def transform(self, param):
+        return param
+
+    def inverse_transform(self, param):
+        return param
