474d679f1b56f9c720bec2d71e20f3b78ddb8385
Pueh Leng Tan <puehleng.tan@gmail.com>
2026-03-23 18:34:28 -0400

    num_sims as input arg as well

--- notebooks/run_sim.sh
+++ notebooks/run_sim.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+num_sims=5
+
 # Loop from 1 to 10
 for ii in {1..10}
 do
     echo "Running with argument: $ii"
-    python sim_nr_realistic.py $ii > "apt_sims_mcind${ii}.log" 2>&1 &
+    python sim_nr_realistic.py $ii $num_sims> "apt_10params_${num_sims}sims_mcind${ii}.log" 2>&1 &
 done
 
 wait
