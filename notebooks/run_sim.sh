3dc865b2298adb8f2c77a049ac2ea366dba3c9c2
Pueh Leng Tan <puehleng.tan@gmail.com>
2026-05-01 19:58:42 -0400

    6-param model uniform g1 proposal prior

--- notebooks/run_sim.sh
+++ notebooks/run_sim.sh
@@ -4,8 +4,10 @@ num_sims=20000
 
 #f_param_config='/home/puehlengt/appletree/notebooks/param_nr_sr1_6params.json'
 #param_flavour='6params'
-f_param_config='/home/puehlengt/appletree/notebooks/param_nr_sr1_3params_uniformg1.json'
-param_flavour='3params_uniformg1'
+f_param_config='/home/puehlengt/appletree/notebooks/param_nr_sr1_6params_uniformg1.json'
+param_flavour='6params_uniformg1'
+#f_param_config='/home/puehlengt/appletree/notebooks/param_nr_sr1_3params_uniformg1.json'
+#param_flavour='3params_uniformg1'
 
 # Loop from 1 to 10
 for ii in {1..10}
---
+++ notebooks/run_sim_testset.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+num_sims=2000
+
+f_param_config='/home/puehlengt/appletree/notebooks/param_nr_sr1_6params.json'
+param_flavour='6params'
+#f_param_config='/home/puehlengt/appletree/notebooks/param_nr_sr1_3params.json'
+#param_flavour='3params'
+
+# Loop from 1 to 10
+#for ii in {1..10}
+for ii in {10001..10010}
+do
+    echo "Running with argument: $ii"
+    python sim_nr_realistic.py $ii $num_sims $f_param_config $param_flavour > "apt_${param_flavour}_${num_sims}sims_mcind${ii}.log" 2>&1 &
+done
+
+wait
+echo "All jobs done!"
