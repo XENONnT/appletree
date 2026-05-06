078c30aaf0001ada94a0779350b6849abbeacae6
Pueh Leng Tan <puehleng.tan@gmail.com>
2026-05-06 19:42:11 -0400

    1param model. only float g1

--- notebooks/run_sim.sh
+++ notebooks/run_sim.sh
@@ -4,10 +4,20 @@ num_sims=20000
 
 #f_param_config='/home/puehlengt/appletree/notebooks/param_nr_sr1_6params.json'
 #param_flavour='6params'
-f_param_config='/home/puehlengt/appletree/notebooks/param_nr_sr1_6params_uniformg1.json'
-param_flavour='6params_uniformg1'
+
+# f_param_config='/home/puehlengt/appletree/notebooks/param_nr_sr1_6params_uniformg1.json'
+# param_flavour='6params_uniformg1'
+
 #f_param_config='/home/puehlengt/appletree/notebooks/param_nr_sr1_3params_uniformg1.json'
 #param_flavour='3params_uniformg1'
+
+# for gaussian prior                                                          
+#f_param_config='/home/puehlengt/appletree/notebooks/param_nr_sr1_1param_normg1.json'
+#param_flavour='1param_normg1'                                                 
+                                                                                 
+# for uniform prior
+f_param_config='/home/puehlengt/appletree/notebooks/param_nr_sr1_1param_uniformg1.json'       
+param_flavour='1param_uniformg1'
 
 # Loop from 1 to 10
 for ii in {1..10}
--- notebooks/run_sim_testset.sh
+++ notebooks/run_sim_testset.sh
@@ -2,10 +2,13 @@
 
 num_sims=200
 
-f_param_config='/home/puehlengt/appletree/notebooks/param_nr_sr1_6params.json'
-param_flavour='6params'
+#f_param_config='/home/puehlengt/appletree/notebooks/param_nr_sr1_6params.json'
+#param_flavour='6params'
 #f_param_config='/home/puehlengt/appletree/notebooks/param_nr_sr1_3params.json'
 #param_flavour='3params'
+f_param_config='/home/puehlengt/appletree/notebooks/param_nr_sr1_1param_normg1.json'
+param_flavour='1param_normg1_testset'
+
 
 # Loop from 1 to 10
 #for ii in {1..10}
