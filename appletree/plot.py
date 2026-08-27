e8545cdb38a282d411f6307b2ed8a17ad4915c7e
Daniel Wenz <43881800+WenzDaniel@users.noreply.github.com>
2026-08-27 18:08:56 +0200

    Add median and +/- 1 sigma quantiles as info (#225)
    * Add median and +/- 1 sigma quantiles as info
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Changed number of digits
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Change format to .4g
    
    * Also add the mpe values
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Also add label
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add new plot for parameter convergence
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Fix edge case with to few iterations
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Fixed bug
    
    * Change label color
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- appletree/plot.py
+++ appletree/plot.py
@@ -19,7 +19,7 @@ from appletree.component import ComponentSim
 class Plotter:
     min_autocorr_time = 100
 
-    def __init__(self, backend_file_name, discard=0, thin=1):
+    def __init__(self, backend_file_name, discard=0, thin=1, plot_posterior_values=False):
         """Plotter for the MCMC chain.
 
         Args:
@@ -30,6 +30,7 @@ class Plotter:
         """
         self.discard = discard
         self.thin = thin
+        self.plot_posterior_values = plot_posterior_values
 
         self.backend_file_name = backend_file_name
         backend = emcee.backends.HDFBackend(self.backend_file_name, read_only=True)
@@ -62,10 +63,16 @@ class Plotter:
 
         self.n_iter, self.n_walker, self.n_param = self.chain.shape
 
-    def make_all_plots(self, save=False, save_path=".", fmt=["png", "pdf"], **save_kwargs):
+    def make_all_plots(
+        self, save=False, save_path=".", fmt=["png", "pdf"], last_n_iterations=100, **save_kwargs
+    ):
         """Make all plots and save them if save is True.
 
         The plot styles are default. save_kwargs will be passed to fig.savefig().
+
+        Args:
+            last_n_iterations: Number of iterations to be used for shaded bands in
+                self.plot_parameter_burn.
 
         """
 
@@ -78,6 +85,10 @@ class Plotter:
         fig, axes = self.plot_burn_in()
         if save:
             save_fig(fig, "burn_in", fmt)
+
+        fig, axes = self.plot_parameter_burn(last_n_iterations=last_n_iterations)
+        if save:
+            save_fig(fig, "last_n_iterations", fmt)
 
         fig, axes = self.plot_marginal_posterior()
         if save:
@@ -199,8 +210,24 @@ class Plotter:
 
         axes = []
         for i in range(self.n_param):
+
             ax = fig.add_subplot(n_rows, n_cols, i + 1)
-            ax.hist(self.flat_chain[:, i], density=True, label="Posterior", **hist_kwargs)
+            samples = self.flat_chain[:, i]
+            if self.plot_posterior_values:
+                median = np.percentile(samples, 50)
+                lower = np.percentile(samples, 15.8655)  # -1 sgma
+                upper = np.percentile(samples, 84.1345)  # +1 sigma
+                msigma = median - lower
+                psigma = upper - median
+
+                ax.axvline(median, color="k")
+                ax.axvline(lower, color="k", ls="dashed")
+                ax.axvline(upper, color="k", ls="dashed")
+                ax.axvline(self.param_mpe[self.param_names[i]], color="orange", label="Best fit")
+                ax.set_title(f"Posterior: {median:.4g} +{psigma:.4g}/-{msigma:.4g}")
+
+            ax.hist(samples, density=True, label="Posterior", **hist_kwargs)
+
             prior = self.param_prior[self.param_names[i]]
             prior_type = prior["prior_type"]
             args = prior["prior_args"]
@@ -446,6 +473,105 @@ class Plotter:
         ax.set_ylim(0, 1)
         ax.set_xlabel("Walker Index")
         ax.legend()
+
+        plt.tight_layout()
+        return fig, axes
+
+    def plot_parameter_burn(self, fig=None, last_n_iterations=100):
+        """Plots the median and standard deviation of all walker per iteration. Further, displays
+        the mean and spread of the last n iterations as a shaded band for median and standard
+        deviation. This helps to better judge if a parameter already converged decently or if the
+        fit should be run for a few more iterations.
+
+        Args:
+            fig: he figure to plot on. If None, a new figure will be created.
+            last_n_iterations: Last N iterations which sould be use for the
+                averaging and shaded bands.
+
+        """
+        _has_enough_iterations = self.n_iter > last_n_iterations
+        if not _has_enough_iterations:
+            last_n_iterations = self.n_iter
+            warn(
+                f"Your fit has less than {last_n_iterations}."
+                '"plot_parameter_burn" will use all iterations for '
+                "averaging instead."
+            )
+
+        n_cols = 2
+        n_rows = int(np.ceil(self.n_param / n_cols))
+
+        if fig is None:
+            fig = plt.figure(figsize=(10, 2 * n_rows))
+
+        axes = []
+        for i in range(self.n_param):
+            ax = fig.add_subplot(n_rows, n_cols, i + 1)
+
+            median = np.median(self.chain[:, :, i], axis=1)
+            median_average_last_n = np.mean(median[-last_n_iterations:])
+            median_width_last_n = np.std(median[-last_n_iterations:])
+
+            std = np.std(self.chain[:, :, i], axis=1)
+            std_average_last_n = np.mean(std[-last_n_iterations:])
+            std_width_last_n = np.std(std[-last_n_iterations:])
+
+            n_iterations = len(median)
+            ax.plot(
+                np.arange(n_iterations - last_n_iterations),
+                median[:-last_n_iterations],
+                color="#4067b1",
+                alpha=0.4,
+            )
+            ax.plot(
+                np.arange(n_iterations - last_n_iterations, n_iterations),
+                median[-last_n_iterations:],
+                color="#4067b1",
+                label="Median",
+            )
+            ax.axhline(median_average_last_n, color="#4067b1")
+            ax.axhspan(
+                median_average_last_n - median_width_last_n,
+                median_average_last_n + median_width_last_n,
+                color="#4067b1",
+                alpha=0.3,
+            )
+
+            ax2 = ax.twinx()
+            ax2.plot(
+                np.arange(n_iterations - last_n_iterations),
+                std[:-last_n_iterations],
+                label="$\\sigma$",
+                color="#6ccef5",
+                alpha=0.4,
+            )
+            ax2.plot(
+                np.arange(n_iterations - last_n_iterations, n_iterations),
+                std[-last_n_iterations:],
+                color="#6ccef5",
+            )
+            ax2.axhline(std_average_last_n, color="#6ccef5")
+            ax2.axhspan(
+                std_average_last_n - std_width_last_n,
+                std_average_last_n + std_width_last_n,
+                color="#6ccef5",
+                alpha=0.3,
+            )
+            ax.set_xlabel("Number of iterations")
+            ax.set_ylabel("Median", color="#4067b1")
+            ax2.set_ylabel("$\\sigma$", color="#6ccef5")
+            ax.set_title(self.param_names[i])
+            axes.append(ax)
+
+        # Set legend
+        handles, labels = ax.get_legend_handles_labels()
+        handles2, labels2 = ax2.get_legend_handles_labels()
+        fig.legend(
+            loc="lower center",
+            handles=list(np.concatenate([handles, handles2])),
+            labels=list(np.concatenate([labels, labels2])),
+            bbox_to_anchor=(0.5, 1.0),
+        )
 
         plt.tight_layout()
         return fig, axes
